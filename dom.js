52723812164b62791c10327ad5cfd1bddc9b77b0
jkomoefey23e@yahoo.co.uk <jkomoefey23e@yahoo.co.uk>
2022-06-16 13:05:46 -0700

    DOM work through

--- dom.js
+++ dom.js
@@ -1,1 +1,59 @@
- 
+//  console.dir(document)
+
+// console.log(document.domain)
+// console.log(document.URL)
+// console.log(document.title)
+// document.title = 234
+// console.log(document.doctype)
+// console.log(document.head)
+// console.log(document.body)
+// console.log(document.all)
+// console.log(document.all[0])
+// document.all[0].textContent = 'Welcome';
+// console.log(document.forms[0]);
+// console.log(document.links);
+// console.log(document.images);
+
+// GETELEMENTBYID
+// console.log(document.getElementById('header-title'));
+var headerTitle = document.getElementById('header-title');
+var header = document.getElementById('main-header');
+// console.log(headerTitle);
+// headerTitle.textContent = 'Welcome';
+// headerTitle.innerText = 'Welcome to our';
+// console.log(headerTitle.innerText);
+// headerTitle.innerHTML = '<h3>Hello<h3>';
+// header.style.borderBottom = 'solid 5px red';
+
+// GETELEMENTBYCLASSNAME
+// var items  = document.getElementsByClassName('list-group-item');
+// console.log(items);
+// console.log(items[1]);
+// items[0].textContent = 'Hello World';
+// items[0].style.fontWeight = 'bold';
+// items[0].style.backgroundColor = 'red';
+// items[0].style.color = 'white';
+
+// for(var i = 0; i < items.length; i++){
+//     items[i].style['background-color'] = 'red';
+// }
+
+
+// GETELEMENTsBYTAGNAME
+var li  = document.getElementsByTagName('li');
+// console.log(li);
+// console.log(li[1]);
+// li[0].textContent = 'Hello World';
+// li[0].style.fontWeight = 'bold';
+// li[0].style.backgroundColor = 'red';
+// li[0].style.color = 'white';
+
+// for(var i = 0; i < li.length; i++){
+//     li[i].style['background-color'] = 'blue';
+// }
+
+
+
+
+
+
