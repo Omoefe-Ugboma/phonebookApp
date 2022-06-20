ce4cba767ee3bb939d8e5113b83b71d63c071900
jkomoefey23e@yahoo.co.uk <jkomoefey23e@yahoo.co.uk>
2022-06-20 14:09:45 -0700

    Class on Dom day 2

--- dom.js
+++ dom.js
@@ -16,8 +16,8 @@
 
 // GETELEMENTBYID
 // console.log(document.getElementById('header-title'));
-var headerTitle = document.getElementById('header-title');
-var header = document.getElementById('main-header');
+// var headerTitle = document.getElementById('header-title');
+// var header = document.getElementById('main-header');
 // console.log(headerTitle);
 // headerTitle.textContent = 'Welcome';
 // headerTitle.innerText = 'Welcome to our';
@@ -40,7 +40,7 @@ var header = document.getElementById('main-header');
 
 
 // GETELEMENTsBYTAGNAME
-var li  = document.getElementsByTagName('li');
+// var li  = document.getElementsByTagName('li');
 // console.log(li);
 // console.log(li[1]);
 // li[0].textContent = 'Hello World';
@@ -51,6 +51,68 @@ var li  = document.getElementsByTagName('li');
 // for(var i = 0; i < li.length; i++){
 //     li[i].style['background-color'] = 'blue';
 // }
+
+// QuerySELECTOR 
+// var Header = document.querySelector('main-header');
+// Header.style.borderBottom = 'solid 4px red';
+
+// var input = document.querySelector('input');
+// input.value = 'Hellow World';
+
+// var submit = document.querySelector('input[type="submit"]');
+// submit.value = "Ebuka";
+
+// var item = document.querySelector('.list-group-item');
+// item.style.backgroundColor = "red"; 
+
+// var lastItem = document.querySelector
+// ('.list-group-item:last-child');
+// lastItem.style.backgroundColor = "red";
+
+// var lastItem = document.querySelector
+// ('.list-group-item:nth-child(1)');
+// lastItem.style.backgroundColor = "red";
+
+// QuerySelectorALL
+// var titles = document.querySelectorAll
+// ('.title');
+// titles[0].textContent = 'Hello';
+
+// var odd = document.querySelectorAll
+// ('li:nth-child(odd)');
+// var even = document.querySelectorAll
+// ('li:nth-child(even)');
+
+// odd.style.backgroundColor = 'red';
+// for(var i = 0; i < odd.length; i++){
+//     odd[i].style.backgroundColor = 'red';
+//     even[i].style.backgroundColor = 'green';
+// }
+
+var newDiv = document.createElement('div');
+
+// add a class
+newDiv.className = 'Ebuka';
+
+newDiv.id = 'Solomon';
+
+newDiv.setAttribute('title', 'Hello Div');
+
+// create text node
+var newDivText = document.createTextNode('Welcome to Nigeria');
+
+// add text to div
+newDiv.appendChild(newDivText);
+
+console.log(newDiv);
+
+
+
+
+
+
+
+
 
 
 
